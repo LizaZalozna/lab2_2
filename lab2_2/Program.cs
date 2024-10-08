138df53e849230b68bc3f8e5dcb8e440104ddbea
LizaZalozna <144709713+LizaZalozna@users.noreply.github.com>
2024-10-08 16:39:52 +0300

    first commit

---
+++ lab2_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Internal;
+
+namespace lab2_2
+{
+    class MyTime
+    {
+        private int hour, minute, second;
+
+        public MyTime(int h, int m, int s)
+        {
+            this.hour = h;
+            this.minute = m;
+            this.second = s;
+        }
+
+        public override string ToString()
+        {
+            return $"{hour:D2}:{minute:D2}:{second:D2}";
+        }
+
+        public int TimeSinceMidnight()
+        {
+            return hour * 3600 + minute * 60 + second;
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(new MyTime(2, 2, 2));
+            Console.ReadKey();
+        }
+    }
+}
